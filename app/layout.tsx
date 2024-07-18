17dfd1270c6c42ac61aec215e7faccf1810a4a13
rowgregory <rowgregory@gmail.com>
2024-07-18 11:33:34 -0400

    Add open graph meta data tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,21 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: `Devon Hunt - Realtor ®`,
   description: "Massachusetts Realtor",
+  metadataBase: new URL('https://www.devonhuntrealtor.com/'),
+  openGraph: {
+    type: 'website',
+    url: 'https://www.devonhuntrealtor.com/',
+    title: 'Devon Hunt - Realtor ®',
+    description: 'Massachusetts Realtor',
+    images: [
+      {
+        url: 'https://firebasestorage.googleapis.com/v0/b/devon-hunt-nextjs.appspot.com/o/images%2Fdevon-rich-preview.png?alt=media&token=0c1ec1c7-f7db-4024-9156-dc5848fedfa1',
+        width: 1200,
+        height: 630,
+        alt: 'Devon Hunt - Realtor ®',
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
