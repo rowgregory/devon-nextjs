ff4e062be13d99dc389720d7986db7377cf30d26
rowgregory <rowgregory@gmail.com>
2024-09-18 19:37:23 -0400

    Fixed background test

--- app/components/FixedBanner.tsx
+++ app/components/FixedBanner.tsx
@@ -2,17 +2,21 @@ import React from "react";
 
 const FixedBanner = ({ bgImg, title, subtitle }: any) => {
   return (
-    <div className="relative w-full h-[450px]">
+    <div className="relative w-full h-[300px] sm:h-[450px]">
       <div
-        className="bg-center bg-cover bg-fixed absolute inset-0"
+        className="bg-cover bg-top bg-fixed absolute inset-0"
         style={{
           backgroundImage: `url(${bgImg})`,
+          backgroundSize: "140%",
+          backgroundRepeat: "no-repeat",
         }}
       />
       <div className="absolute inset-0 bg-black/50" />
-      <div className="absolute inset-0 flex flex-col items-center justify-center z-10">
-        <h1 className="font-bold text-5xl text-white text-center">{title}</h1>
-        <h4 className="pt-7 lg:pt-9 pb-7 font-bold text-lg text-white tracking-wider">
+      <div className="absolute inset-0 flex flex-col items-center justify-center p-4 sm:p-6 z-10">
+        <h1 className="text-4xl sm:text-5xl font-bold text-white text-center">
+          {title}
+        </h1>
+        <h4 className="pt-4 sm:pt-7 pb-4 sm:pb-7 font-bold text-base sm:text-lg text-white tracking-wider text-center">
           {subtitle}
         </h4>
       </div>
